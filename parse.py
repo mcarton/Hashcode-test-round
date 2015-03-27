f6f218903516a919187be1ba98c1d287f14071d2
Maxime Arthaud <maxime@arthaud.me>
2015-03-27 21:14:31 +0100

    Recherche du jambon le plus proche

--- parse.py
+++ parse.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 
 
@@ -9,6 +10,9 @@ class Problem:
         self.s = s
         self.problem = problem
 
+    def coord_valid(self, x, y):
+        return x >= 0 and x < self.rows and y >= 0 and y < self.cols
+
 
 def parse(file):
         rows, cols, h, s = map(int, file.readline().strip().split())
@@ -19,5 +23,36 @@ def parse(file):
 
         return Problem(rows, cols, h, s, problem)
 
-with open(sys.argv[1], 'r') as f:
-    print(parse(f).problem)
+
+def found_slices(p):
+    slices = []
+
+    for i in range(p.rows):
+        for j in range(p.cols):
+            if p.problem[i][j] == 'H':
+                # on cherche le jambon le plus proche
+                c = 1
+                while c < 3:
+                    for x in range(j - c, j + c + 1):
+                        # ligne du haut
+                        if p.coord_valid(i - c, x) and p.problem[i - c][x] == 'H':
+                            print('ligne haut i=%d j=%d c=%d jambon en (%d, %d)' % (i, j, c, i - c, x))
+
+                        # ligne du bas
+                        if p.coord_valid(i + c, x) and p.problem[i + c][x] == 'H':
+                            print('ligne bas i=%d j=%d c=%d jambon en (%d, %d)' % (i, j, c, i + c, x))
+
+                    for y in range(i - c, i + c + 1):
+                        # colonne gauche
+                        if p.coord_valid(y, j - c) and p.problem[y][j - c] == 'H':
+                            print('colonne gauche i=%d j=%d c=%d jambon en (%d, %d)' % (i, j, c, y, j - c))
+
+                        # colonne droite
+                        if p.coord_valid(y, j + c) and p.problem[y][j + c] == 'H':
+                            print('colonne droite i=%d j=%d c=%d jambon en (%d, %d)' % (i, j, c, y, j + c))
+                    c += 1
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as f:
+        p = parse(f)
+        found_slices(p)
