28965101cd1a40953dc17463e37570fa8989046d
Maxime Arthaud <maxime@arthaud.me>
2015-03-27 21:19:26 +0100

    Ajout d'une petite fonction

--- parse.py
+++ parse.py
@@ -24,33 +24,44 @@ def parse(file):
         return Problem(rows, cols, h, s, problem)
 
 
+def found_closest_ham(p, i, j):
+    ''' Retourne la liste des jambons les plus proches de (i, j) '''
+    c = 1
+    while True:
+        positions = []
+
+        for x in range(j - c, j + c + 1):
+            # ligne du haut
+            if p.coord_valid(i - c, x) and p.problem[i - c][x] == 'H':
+                positions.append((i - c, x))
+
+            # ligne du bas
+            if p.coord_valid(i + c, x) and p.problem[i + c][x] == 'H':
+                positions.append((i + c, x))
+
+        for y in range(i - c, i + c + 1):
+            # colonne gauche
+            if p.coord_valid(y, j - c) and p.problem[y][j - c] == 'H':
+                positions.append((y, j - c))
+
+            # colonne droite
+            if p.coord_valid(y, j + c) and p.problem[y][j + c] == 'H':
+                positions.append((y, j + c))
+
+        if positions:
+            return positions
+
+        c += 1
+
+
 def found_slices(p):
     slices = []
 
     for i in range(p.rows):
         for j in range(p.cols):
             if p.problem[i][j] == 'H':
-                # on cherche le jambon le plus proche
-                c = 1
-                while c < 3:
-                    for x in range(j - c, j + c + 1):
-                        # ligne du haut
-                        if p.coord_valid(i - c, x) and p.problem[i - c][x] == 'H':
-                            print('ligne haut i=%d j=%d c=%d jambon en (%d, %d)' % (i, j, c, i - c, x))
+                print('(%d, %d) closests = %s' % (i, j, found_closest_ham(p, i, j)))
 
-                        # ligne du bas
-                        if p.coord_valid(i + c, x) and p.problem[i + c][x] == 'H':
-                            print('ligne bas i=%d j=%d c=%d jambon en (%d, %d)' % (i, j, c, i + c, x))
-
-                    for y in range(i - c, i + c + 1):
-                        # colonne gauche
-                        if p.coord_valid(y, j - c) and p.problem[y][j - c] == 'H':
-                            print('colonne gauche i=%d j=%d c=%d jambon en (%d, %d)' % (i, j, c, y, j - c))
-
-                        # colonne droite
-                        if p.coord_valid(y, j + c) and p.problem[y][j + c] == 'H':
-                            print('colonne droite i=%d j=%d c=%d jambon en (%d, %d)' % (i, j, c, y, j + c))
-                    c += 1
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r') as f:
