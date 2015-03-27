d532af29435d7022a954fe0ad67a7c654a323806
Maxime Arthaud <maxime@arthaud.me>
2015-03-27 21:44:12 +0100

    Cleanup

--- parse.py
+++ parse.py
@@ -13,6 +13,43 @@ class Problem:
     def coord_valid(self, x, y):
         return x >= 0 and x < self.rows and y >= 0 and y < self.cols
 
+    def found_closest_ham(self, i, j):
+        ''' Retourne la liste des jambons les plus proches de (i, j) '''
+        c = 1
+        while True:
+            positions = []
+
+            for x in range(j - c, j + c + 1):
+                # ligne du haut
+                if self.coord_valid(i - c, x) and self.problem[i - c][x] == 'H':
+                    positions.append((i - c, x))
+
+                # ligne du bas
+                if self.coord_valid(i + c, x) and self.problem[i + c][x] == 'H':
+                    positions.append((i + c, x))
+
+            for y in range(i - c, i + c + 1):
+                # colonne gauche
+                if self.coord_valid(y, j - c) and self.problem[y][j - c] == 'H':
+                    positions.append((y, j - c))
+
+                # colonne droite
+                if self.coord_valid(y, j + c) and self.problem[y][j + c] == 'H':
+                    positions.append((y, j + c))
+
+            if positions:
+                return positions
+
+            c += 1
+
+    def count_ham(self, topleft_x, topleft_y, bottomright_x, bottomright_y):
+        c = 0
+        for x in range(topleft_x, bottomright_x + 1):
+            for y in range(topleft_y, bottomright_y + 1):
+                if self.problem[x][y] == 'H':
+                    c += 1
+
+        return c
 
 def parse(file):
         rows, cols, h, s = map(int, file.readline().strip().split())
@@ -24,46 +61,6 @@ def parse(file):
         return Problem(rows, cols, h, s, problem)
 
 
-def found_closest_ham(p, i, j):
-    ''' Retourne la liste des jambons les plus proches de (i, j) '''
-    c = 1
-    while True:
-        positions = []
-
-        for x in range(j - c, j + c + 1):
-            # ligne du haut
-            if p.coord_valid(i - c, x) and p.problem[i - c][x] == 'H':
-                positions.append((i - c, x))
-
-            # ligne du bas
-            if p.coord_valid(i + c, x) and p.problem[i + c][x] == 'H':
-                positions.append((i + c, x))
-
-        for y in range(i - c, i + c + 1):
-            # colonne gauche
-            if p.coord_valid(y, j - c) and p.problem[y][j - c] == 'H':
-                positions.append((y, j - c))
-
-            # colonne droite
-            if p.coord_valid(y, j + c) and p.problem[y][j + c] == 'H':
-                positions.append((y, j + c))
-
-        if positions:
-            return positions
-
-        c += 1
-
-
-def found_slices(p):
-    slices = []
-
-    for i in range(p.rows):
-        for j in range(p.cols):
-            if p.problem[i][j] == 'H':
-                print('(%d, %d) closests = %s' % (i, j, found_closest_ham(p, i, j)))
-
-
 if __name__ == '__main__':
     with open(sys.argv[1], 'r') as f:
         p = parse(f)
-        found_slices(p)
